aa59d8ec5c0be91077594376d67c34ff8218ee06
Jason Cox <coxj@vt.edu>
2018-11-01 14:55:38 -0400

    Added Database and Model

---
+++ NetworkMapData/Partials/Port.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkMapData
+{
+    public partial class Port
+    {
+        /// <summary>
+        /// Ensures a reflexive relationship between connected ports a and b.
+        /// If, either a or b is already connected to another port, this connection is severed reflexively as well.
+        /// be sure to save changes after modifying ports!
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        public static void ConnectPorts(ref Port a, ref Port b)
+        {
+            if (a.ConnectedPort != null)
+            {
+                a.ConnectedPort.ConnectedPort = null;
+            }
+            if (b.ConnectedPort != null)
+            {
+                b.ConnectedPort.ConnectedPort = null;
+            }
+
+            a.ConnectedPort = b;
+            b.ConnectedPort = a;
+        }
+    }
+}
---
+++ NetworkMapData/Partials/Room.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetworkMapData
+{
+    public partial class Room
+    {
+        /// <summary>
+        /// Readonly Accessor for ports in this room.
+        /// This room's ports cannot be modified through this enumeration.
+        /// To add a port to a room, it must be added to a PortGroup in the room.
+        /// </summary>
+        public List<Port> Ports
+        {
+            get
+            {
+                List<Port> ports = new List<Port>();
+                foreach(PortGroup grp in this.PortGroups)
+                {
+                    ports.AddRange(grp.Ports);
+                }
+
+                return ports;
+            }
+        }
+    }
+}

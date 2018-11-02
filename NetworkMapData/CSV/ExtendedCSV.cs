44ba639e90482f173a7086eee2daae7b2aac4608
Jason Cox <coxj@vt.edu>
2018-11-02 09:07:41 -0400

    Added CSV to the project.

---
+++ NetworkMapData/CSV/ExtendedCSV.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.IO;
+
+namespace CSV
+{
+    /// <summary>
+    /// Extention of the CSV class which can be used for various data manipulation.
+    /// 
+    /// Currently implements flattening of CSV files given a list of primary keys.
+    /// </summary>
+    public class ExtendedCSV : CSV
+    {
+        /// <summary>
+        /// Gets or sets the list of unique fields.
+        /// This might be the primary keys for the table.
+        /// </summary>
+        /// <value>The unique fields.</value>
+        public List<String> UniqueFields
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// The conflict rule.
+        /// </summary>
+        public IConflictRule ConflictRule;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:CSVDataManipulation.ExtendedCSV"/> class.
+        /// </summary>
+        /// <param name="inputStream">Input stream.</param>
+        /// <param name="uidfields">Uidfields.</param>
+        public ExtendedCSV(Stream inputStream, List<string> uidfields = null) : base(inputStream)
+        {
+            UniqueFields = uidfields == null ? new List<string>() : uidfields;
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:CSVDataManipulation.ExtendedCSV"/> class.
+        /// </summary>
+        /// <param name="toExtend">To extend.</param>
+        /// <param name="uidfields">Uidfields.</param>
+        public ExtendedCSV(CSV toExtend, List<string> uidfields) : base(toExtend.Data)
+        {
+            this.UniqueFields = uidfields;
+        }
+
+        /// <summary>
+        /// Compares the unique fields.
+        /// </summary>
+        /// <returns><c>true</c>, if unique fields was compared, <c>false</c> otherwise.</returns>
+        /// <param name="rowA">Row a.</param>
+        /// <param name="rowB">Row b.</param>
+        protected bool CompareUniqueFields(Dictionary<string, string> rowA, Dictionary<String, String> rowB)
+        {
+            foreach (String uid in UniqueFields)
+            {
+                if (!rowA.ContainsKey(uid) || !rowB.ContainsKey(uid))
+                    return false;
+                if (!rowA[uid].Equals(rowB[uid]))
+                    return false;
+
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Find the *first* row with specified primaryKey.
+        /// Ideally, this should be the *unique* row with the given primaryKey,
+        /// but this method does not check that.
+        /// 
+        /// throws <see cref="System.Collections.Generic.KeyNotFoundException"/> if no match is found.
+        /// throws <see cref="InvalidDataException"/> if the primary key does not contain the appropriate unique fields.
+        /// </summary>
+        /// <returns>The the row that matches the given primary key or throws <see cref="System.Collections.Generic.KeyNotFoundException"/>.</returns>
+        /// <param name="primaryKey">Primary key.</param>
+        public Dictionary<string, string> Find(Dictionary<string, string> primaryKey)
+        {
+            foreach(Dictionary<string, string> row in this._Data)
+            {
+                bool match = true;
+                foreach(String pk in UniqueFields)
+                {
+                    if (!primaryKey.ContainsKey(pk)) 
+                        throw new InvalidDataException("Provided primaryKey does not contain a required field.");
+                    
+                    if(!row[pk].Equals(primaryKey[pk]))
+                    {
+                        match = false;
+                        break;
+                    }
+                }
+
+                if (match)
+                    return row;
+            }
+
+            throw new KeyNotFoundException("No row corresponds to the given primary key.");
+        }
+
+        new public void Remove(Dictionary<String, String> primaryKey)
+        {
+            try
+            {
+                Dictionary<string, string> match = this.Find(primaryKey);
+                this._Data.Remove(match);
+            }
+            catch (KeyNotFoundException)
+            {
+                
+            }
+            catch(InvalidDataException)
+            {
+                
+            }
+        }
+
+
+        #region flatten file
+        /// <summary>
+        /// Squashs the rows.
+        /// </summary>
+        /// <returns>The rows.</returns>
+        public CSV FlattenRows()
+        {
+            List<Dictionary<String, String>> output = new List<Dictionary<string, string>>();
+
+            for (int i = 0; i < this.Data.Count; i++)
+            {
+                bool merged = false;
+                for (int j = 0; j < output.Count; j++)
+                {
+                    if(CompareUniqueFields(this.Data[i], output[j]))
+                    {
+                        output[j] = Merge(output[j], this.Data[i]);
+                        merged = true;
+                    }
+                }
+
+                if (!merged)
+                    output.Add(this.Data[i]);
+            }
+
+            return new CSV(output);
+        }
+
+
+        /// <summary>
+        /// Merge the specified destination and row.
+        /// </summary>
+        /// <returns>The merge.</returns>
+        /// <param name="destination">Destination.</param>
+        /// <param name="row">Row.</param>
+        protected Dictionary<String,String> Merge(Dictionary<string, string> destination, Dictionary<string, string> row)
+        {
+            List<string> keys = new List<string>();
+
+            foreach (string key in destination.Keys)
+            {
+                keys.Add(key);
+            }
+            foreach(string key in keys)
+            {
+                if(!UniqueFields.Contains(key))
+                {
+                    if (String.IsNullOrWhiteSpace(destination[key]))
+                        destination[key] = row[key];
+                    if (!destination[key].Equals(row[key]) && !String.IsNullOrWhiteSpace(row[key]))
+                        destination[key] = ConflictRule.Resolve(destination, row, key);
+                    
+                }
+            }
+
+            return destination;
+        }
+
+        #endregion // flatten
+
+        #region merge documents
+
+        /// <summary>
+        /// Merge this ExtendedCSV with another CSV.  
+        /// The merger looks for matching unique field rows in the /other/ csv and merges them into this one.
+        /// 
+        /// </summary>
+        /// <param name="other">CSV with additional data.</param>
+        public void Merge(CSV other)
+        {
+            foreach (Dictionary<string, string> rowA in this.Data)
+            {
+                foreach (Dictionary<string, string> rowB in other.Data)
+                {
+                    if(CompareUniqueFields(rowA, rowB))
+                    {
+                        foreach(String column in rowB.Keys)
+                        {
+                            if(!rowA.ContainsKey(column))
+                            {
+                                // add the missing data to this sheet.
+                                rowA.Add(column, rowB[column]);
+                            }
+                            else if(!rowA[column].Equals(rowB[column]))
+                            {
+                                // check for conflicting data.
+                                rowA[column] = ConflictRule.Resolve(rowA, rowB, column);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void GetDataColumnsFrom(ExtendedCSV other, List<String> columnsToPull)
+        {
+            foreach(Dictionary<string, string> sourceRow in this._Data)
+            {
+                Dictionary<string, string> otherRow;
+                try
+                {
+                    otherRow = other.Find(sourceRow);
+                }
+                catch(KeyNotFoundException)
+                {
+                    // ignore
+
+                    // TODO:  Don't ignore this...
+                    continue;
+                }
+
+                foreach(string column in columnsToPull)
+                {
+                    if (!otherRow.ContainsKey(column))
+                        continue;
+
+                    if (sourceRow.ContainsKey(column))
+                        sourceRow[column] = otherRow[column];
+                    else
+                        sourceRow.Add(column, otherRow[column]);
+                }
+            }
+        }
+
+        #endregion // merge documents
+
+        #region Comparison
+
+        /// <summary>
+        /// Compares the other csv to this csv and returns a CSV containing all 
+        /// of the rows that are present in the Other but are missing from this csv.
+        /// </summary>
+        /// <returns>The missing rows from.</returns>
+        /// <param name="other">Other.</param>
+        public CSV GetMissingRowsFrom(CSV other)
+        {
+            CSV output = new CSV();
+
+            foreach(Dictionary<String, String> row in other.Data)
+            {
+                bool found = false;
+                foreach(Dictionary<string, string> checkRow in this.Data)
+                {
+                    if(CompareUniqueFields(checkRow, row))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if(!found)
+                {
+                    output.Add(row);
+                }
+            }
+
+            return output;
+        }
+
+
+        /// <summary>
+        /// Gets the extra rows from this csv which are not present in the other csv.
+        /// </summary>
+        /// <returns>The extra rows from.</returns>
+        /// <param name="other">Other.</param>
+        public CSV GetExtraRowsFrom(CSV other)
+        {
+            CSV output = new CSV();
+            foreach (Dictionary<String, String> row in this.Data)
+            {
+                bool found = false;
+                foreach (Dictionary<string, string> checkRow in other.Data)
+                {
+                    if (CompareUniqueFields(checkRow, row))
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    output.Add(row);
+                }
+            }
+
+            return output;
+        }
+
+        public CSV PullRowsMatchingPrimaryKeysWith(CSV other)
+        {
+            CSV output = new CSV();
+
+            foreach(Dictionary<String, String> row in other.Data)
+            {
+                try
+                {
+                    output.Add(this.Find(row));
+                }
+                catch(KeyNotFoundException)
+                {
+                    continue;
+                }
+                catch(InvalidDataException)
+                {
+                    continue;
+                }
+            }
+
+            return output;
+        }
+
+        #endregion // Comparison
+
+
+        public void NormalizeColumns(INormalizationRule rule, List<String> columns)
+        {
+            rule.Normalize(ref _Data, columns);
+        }
+
+    }
+}
---
+++ NetworkMapData/CSV/NormalizationRule.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace CSV
+{
+    public interface INormalizationRule
+    {
+        void Normalize(ref List<Dictionary<String, String>> data, List<String> columns);
+    }
+
+
+    public class SerialNumberNormalizationRule : INormalizationRule
+    {
+        public bool Capitalize { get; set; }
+
+        /// <summary>
+        /// Normalize the specified columns of the given data.
+        /// Standardizes data as all caps or all lowercase depending on the status of the Capitalize property.
+        /// </summary>
+        /// <param name="data">Data.</param>
+        /// <param name="columns">Columns.</param>
+        public void Normalize(ref List<Dictionary<string, string>> data, List<string> columns)
+        {
+            foreach(Dictionary<string, string> row in data)
+            {
+                foreach(String column in columns)
+                {
+                    row[column] = Capitalize ? row[column].ToUpperInvariant() : row[column].ToLowerInvariant();
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// MAC Address normalization rule.
+    /// </summary>
+    public class MACAddressNormalizationRule : INormalizationRule
+    {
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="T:CSVDataManipulation.MACAddressNormalizationRule"/>
+        /// is all caps.
+        /// </summary>
+        /// <value><c>true</c> if capitalize; otherwise, <c>false</c>.</value>
+        public bool Capitalize { get; set; }
+        public enum MacSeparator
+        {
+            None = 0,
+            Colon = 1,
+            Dash = 2
+        }
+
+        private String[] separators = { "", ":", "-" };
+
+        /// <summary>
+        /// Gets or sets the standard separator.
+        /// </summary>
+        /// <value>The separator.</value>
+        public MacSeparator Separator { get; set; }
+
+        /// <summary>
+        /// Normalize the MAC Addresses in the specified columns of data according to the configured standards.
+        /// </summary>
+        /// <param name="data">Data.</param>
+        /// <param name="columns">Columns.</param>
+        public void Normalize(ref List<Dictionary<string, string>> data, List<string> columns)
+        {
+            Regex macaddress = new Regex(MatchPattern);
+            foreach(Dictionary<String, String> row in data)
+            {
+                foreach(String column in columns)
+                {
+                    if (macaddress.IsMatch(row[column]))
+                        continue;
+
+                    String stripped = "";
+                    Regex digit = new Regex("[0-9a-fA-F]");
+
+                    foreach(char ch in row[column])
+                    {
+                        if(digit.IsMatch("" + ch))
+                        {
+                            stripped += ch;
+                        }
+                    }
+
+                    stripped = Capitalize ? stripped.ToUpperInvariant() : stripped.ToLowerInvariant();
+
+                    if (stripped.Length != 12)
+                        continue;
+                        //throw new FormatException("Invalid MAC Address");
+
+                    if( Separator == MacSeparator.None) 
+                    {
+                        row[column] = stripped;
+                        continue;
+                    }
+
+                    String formatted = "";
+                    for (int i = 0; i < 12; i+=2)
+                    {
+                        formatted += stripped[i];
+                        formatted += stripped[i + 1];
+                        if (i < 9) formatted += separators[(int)Separator];
+                    }
+
+                    row[column] = formatted;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets the match pattern corresponding to a mac address with the 
+        /// configured standard for this instance.
+        /// </summary>
+        /// <value>Regex pattern.</value>
+        protected string MatchPattern
+        {
+            get
+            {
+                string letter = Capitalize ? "A-F" : "a-f";
+                if(Separator == MacSeparator.None)
+                {
+                    return "^[0-9" + letter + "]{12}$";
+                }
+
+                return "^([0-9" + letter + "]{2}" + separators[(int)Separator] + "){5}[" + letter + "0-9]{2}$";
+            }
+        }
+    }
+}
